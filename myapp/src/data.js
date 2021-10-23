0668a44ee9b950e1d5663aa5d3d4d18849ea9973
BrianSaldana <brianxsaldana@gmail.com>
2021-10-22 20:48:21 -0700

    **Both Sliders Working**

--- myapp/src/data.js
+++ myapp/src/data.js
@@ -4,21 +4,21 @@ export const sliderItems = [
         img: 'https://www.freepnglogos.com/uploads/car-png/car-png-large-images-40.png',
         title: 'Cars For College Students',
         desc: "Sign up, Sign in, and get on the road with your new car",
-        bg: 'f5fafd'
+        bg: 'd2b48c'
     },
     {
         id: 2,
         img: 'https://pngimg.com/uploads/bmw/bmw_PNG1675.png',
         title: 'Cars For College Students',
         desc: "Sign up, Sign in, and get on the road with your new car",
-        bg: 'fbf0f4'
+        bg: 'addae6'
     },
     {
         id: 3,
         img: 'https://www.freepnglogos.com/uploads/car-png/download-car-png-image-pngimg-1.png',
         title: 'Cars For College Students',
         desc: "Sign up, Sign in, and get on the road with your new car",
-        bg: 'fcf1ed'
+        bg: 'd3d3d3'
     },
 
 
