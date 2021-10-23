525df86cf34c083c886c3855f6a8f316b109f110
BrianSaldana <brianxsaldana@gmail.com>
2021-10-22 19:34:40 -0700

    1st commit

---
+++ myapp/src/data.js
@@ -0,0 +1,22 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://www.freepnglogos.com/uploads/car-png/car-png-large-images-40.png',
+        title: 'Cars For College Students',
+        bg: 'f5fafd'
+    },
+    {
+        id: 2,
+        img: 'https://pngimg.com/uploads/bmw/bmw_PNG1675.png',
+        title: 'Cars For College Students',
+        bg: 'fbf0f4'
+    },
+    {
+        id: 3,
+        img: 'https://www.freepnglogos.com/uploads/car-png/download-car-png-image-pngimg-1.png',
+        title: 'Cars For College Students',
+        bg: 'fcf1ed'
+    },
+
+
+];
