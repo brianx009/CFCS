356e0484f65198873389baa8e0a9b87f9afcc55f
BrianSaldana <brianxsaldana@gmail.com>
2021-10-22 19:52:19 -0700

    Website Skeleton with working Slider

--- myapp/src/components/Slider.jsx
+++ myapp/src/components/Slider.jsx
@@ -33,7 +33,7 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
     height: 100%;
     display: flex;
-    transform: translate(0vw);
+    transform: translateX(${(props)=> props.slideIndex * -100}vw);
 `
 
 const Slide = styled.div`
@@ -75,14 +75,22 @@ const Button = styled.button`
 `
 const Slider = () => { 
     const [slideIndex, setSlideIndex] = useState(0);
-    const handleClick = (direction) => {};
+    const handleClick = (direction) => {
+
+        if(direction === 'left'){
+            setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2 );
+        }else{
+
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+        }
+    };
 
     return (
         <Container>
             <Arrow direction= 'left' onClick ={() =>handleClick('left')}>
                 <ArrowLeftOutlined/> 
             </Arrow>
-            <Wrapper>
+            <Wrapper slideIndex = {slideIndex}>
                 {sliderItems.map((item)=>(
                     <Slide bg= {item.bg}>
                         <ImgContainer>
