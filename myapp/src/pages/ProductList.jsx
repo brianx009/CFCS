f5c9d5e57523f4cf7000d4ed6000b2ffc39c6f84
BrianSaldana <brianxsaldana@gmail.com>
2021-10-26 17:15:25 -0700

    ** Product List page Complete and Working**

---
+++ myapp/src/pages/ProductList.jsx
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement'
+import Products from '../components/Products'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
+
+const Container = styled.div``
+
+const Title = styled.h1`
+    margin: 20px;
+
+`
+
+const FilterContainer = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const Filter = styled.div`
+    margin: 20px;
+`
+
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+    marigin-right: 20px;
+`
+
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`
+const Option = styled.option``
+
+const ProductList = () => {
+    return (
+        <Container>
+            <Navbar/>
+            <Announcement/>
+            <Title>Inventory</Title>
+            <FilterContainer>
+                <Filter>
+                    <FilterText>Filter Products:</FilterText>
+                    <Select> 
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Grey</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                    </Select>
+                    <Select>
+                        <Option>Sports Cars</Option>
+                        <Option>Economic Cars</Option>
+                        <Option>Trucks</Option>
+                    </Select>
+                </Filter>
+                <Filter>    
+                    <FilterText>Sort Products:</FilterText>
+                    <Select>
+                        <Option selected>Recently Added</Option>
+                        <Option>Price (Low to High)</Option>
+                        <Option>Price (High to Low)</Option>
+
+                    </Select>
+                </Filter>
+            </FilterContainer>
+            <Products/>
+            <Newsletter/>
+            <Footer/>
+        </Container>
+    )
+}
+
+
+export default ProductList
