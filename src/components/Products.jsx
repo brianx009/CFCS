b79c0d38d259ec5d8e18e6ce249b1df6eabe52e2
BrianSaldana <brianxsaldana@gmail.com>
2021-11-22 15:26:31 -0800

    Fixed Backend connection

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,18 +13,22 @@ const Container = styled.div`
 
 const Products = ({cat,filters,sort}) => {
 
-  const [products,setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
 
     const getProducts = async () =>{
       try{
-        const res = await axios.get("http:localhost:5000/api/products");
+        const res = await axios.get(
+          cat
+            ? 'http:localhost:5000/api/products?category=${cat}'
+            : 'http://localhost:5000/api/products'
+            );
         console.log(res);
 
       }catch(err){
-
+          console.log("suck my cock!");
       }
     }
    getProducts();
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,202 +1,231 @@
-import { Add, Remove } from '@material-ui/icons'
-import styled from 'styled-components'
-import Announcement from '../components/Announcement'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import { mobile } from '../responsive'
+import { Add, Remove } from "@material-ui/icons";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { mobile } from "../responsive";
+import StripeCheckout from "react-stripe-checkout";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Container = styled.div`
-
-`
+const KEY =
+  "pk_test_51JpuOxG68LOLDj3pRakY6QlKd3zPM9KdH7WSYdFKZv89Z8OsVboRy3FGLgb9eBs7iP4oCBx95aFDHlFEMO7zJQdE00sl0Ed8dh";
+const Container = styled.div``;
 const Wrapper = styled.div`
-     padding: 20px;
-     ${mobile({padding: '10px'})}
-`
+  padding: 20px;
+  ${mobile({ padding: "10px" })}
+`;
 const Title = styled.h1`
-    font-weight: 300;
-    text-align: center;
-`
+  font-weight: 300;
+  text-align: center;
+`;
 const Top = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 20px;
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
 
 const TopButton = styled.button`
-    padding: 10px;
-    font-weight: 600;
-    cursor: pointer;
-    border: ${props=>props.type === 'filled' && 'none'};
-    background-color: ${props=>props.type === 'filled' ? 'black' : 'transparent'};
-    color: ${props=>props.type === 'filled' && 'white'};
-`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
 const TopTexts = styled.div`
-    ${mobile({display: 'none'})}
-`
+  ${mobile({ display: "none" })}
+`;
 const TopText = styled.span`
-    text-decoration: underline;
-    cursor: pointer;
-    margin: 0px 10px;
-`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
+`;
 
 const Bottom = styled.div`
-    display: flex;
-    justifu-content: space-between;
-    ${mobile({flexDirection: 'column'})}
-`
+  display: flex;
+  justifu-content: space-between;
+  ${mobile({ flexDirection: "column" })}
+`;
 const Info = styled.div`
-    flex: 3;
-`
-const Product  = styled.div`
-    display: flex;
-    justify-content: space-between;
-    ${mobile({flexDirection: 'column'})}
-`
+  flex: 3;
+`;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
+`;
 const ProductDetail = styled.div`
-    flex: 2;
-`
+  flex: 2;
+`;
 const Image = styled.img`
-    width: 200px;
-    ${mobile({width: '100%'})}
-    
-`
+  width: 200px;
+  ${mobile({ width: "100%" })}
+`;
 const Details = styled.div`
-    padding:20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-`
-const ProductName = styled.span`
-    
-`
-const ProductId = styled.span`
-    
-`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const ProductName = styled.span``;
+const ProductId = styled.span``;
 const PriceDetail = styled.span`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    align-contents: center;
-    justify-content: center;
-`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-contents: center;
+  justify-content: center;
+`;
 const ProductAmountContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 20px;
-`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
 const ProductAmount = styled.div`
-    font-size: 24px;
-    margin: 5px;${mobile({margin: '5px 15px'})}
-
-`
+  font-size: 24px;
+  margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
+`;
 const ProductPrice = styled.div`
-    font-size: 30px;
-    font-weight: 200;
-    ${mobile({marginBottom: '20px'})}
-`
+  font-size: 30px;
+  font-weight: 200;
+  ${mobile({ marginBottom: "20px" })}
+`;
 
 const Hr = styled.hr`
-    background-color: #EEE;
-    border: none;
-`
+  background-color: #eee;
+  border: none;
+`;
 
 const Summary = styled.div`
-    flex: 1;
-    border: 0.5px solid lightgray;
-    border-radius: 10px;
-    padding: 20px;
-    height:50vh;
-`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
 
 const SummaryTitle = styled.h1`
-    font-weight: 200;
-`
+  font-weight: 200;
+`;
 const SummaryItem = styled.div`
-    margin: 30px 0px;
-    display: flex;
-    justify-content: space-between;
-    font-weight: ${props=>props.type === 'total' && '500'};
-    font-size: ${props=>props.type === 'total' && '24px'};
-`
-const SummaryItemText = styled.span`
-
-`
-const SummaryItemPrice = styled.span`
-
-`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
 const Button = styled.button`
-     width: 100%;
-     padding: 10px;
-     background-color: black;
-     color: white;
-     font-weight: 600;
-`
-
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
 
 const Cart = () => {
-    return (
-        <Container>
-            <Announcement/>
-            <Navbar/>
-            <Wrapper>
-                <Title>YOUR BAG</Title>
-                <Top>
-                    <TopButton type='filled'>Continue Shopping</TopButton>
-                    <TopTexts>
-                        <TopText> Shopping Bag(1)</TopText>
-                        <TopText>Your Favorites(0)</TopText>
-                    </TopTexts>
-                    <TopButton>Check Out</TopButton>
-                </Top>
-                <Bottom>
+  const [stripeToken, setStripeToken] = useState(null);
 
-                    <Info>
-                        <Product>
-                            <ProductDetail>
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
 
-                                <Image src = 'https://www.thegroupnc.com/wp-content/uploads/2017/12/Honda.png'/>
-                                <Details>
-                                    <ProductName><b>Product:</b> 2017 Honda Accord </ProductName>
-                                    <ProductId><b>ID:</b> 63874638512 </ProductId>
-                                </Details>
-                            </ProductDetail>
-                            <PriceDetail>
-                                <ProductAmountContainer>
-                                    <Add/>
-                                    <ProductAmount>1</ProductAmount>
-                                    <Remove/>
-                                </ProductAmountContainer>
-                                <ProductPrice>$9,999</ProductPrice>
-                            </PriceDetail>
-                            <Hr/>
-                        </Product>
-                    </Info>
-                    <Summary>
-                        <SummaryTitle>Order Summary</SummaryTitle>
-                        <SummaryItem>
-                            <SummaryItemText>Subtotal</SummaryItemText>
-                            <SummaryItemPrice>$9,999</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText>Estimated Shipping</SummaryItemText>
-                            <SummaryItemPrice>$399.99</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText>Shipping Discount</SummaryItemText>
-                            <SummaryItemPrice>$-399.99</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem type='total'>
-                            <SummaryItemText>Total</SummaryItemText>
-                            <SummaryItemPrice>$9,999</SummaryItemPrice>
-                        </SummaryItem>
-                        <Button>CHECKOUT NOW</Button>
-                    </Summary>
-                </Bottom>
-            </Wrapper>
-            <Footer/>
-        </Container>
-    )
-}
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await axios.post(
+          "http://localhost:5000/api/checkout/payment",
+          {
+            tokenId: stripeToken.id,
+            amount: 9999900,
+          }
+        );
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    //stripeToken && makeRequest;
+  }, [stripeToken]);
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton type="filled">Continue Shopping</TopButton>
+          <TopTexts>
+            <TopText> Shopping Bag(1)</TopText>
+            <TopText>Your Favorites(0)</TopText>
+          </TopTexts>
+          <TopButton>Check Out</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://www.thegroupnc.com/wp-content/uploads/2017/12/Honda.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> 2017 Honda Accord{" "}
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 63874638512{" "}
+                  </ProductId>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$9,999</ProductPrice>
+              </PriceDetail>
+              <Hr />
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$9,999</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$399.99</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$-399.99</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$9,999</SummaryItemPrice>
+            </SummaryItem>
+            <StripeCheckout
+              name="CFCS"
+              billingAddress
+              shippingAddress
+              description="Your total is 9,999.00"
+              amount={999900}
+              token={onToken}
+              stripeKey={KEY}
+            >
+              <Button>CHECKOUT NOW</Button>
+            </StripeCheckout>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
 
 export default Cart;
